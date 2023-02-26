4458f3cbf63c69969724787f7fae775c513b7488
vicsanesp <vse1997@gmail.com>
2023-02-26 19:14:19 +0100

    finalización del index-VSE

--- index-VSE.js
+++ index-VSE.js
@@ -12,3 +12,14 @@
 
 // console.log("Server ready in port 12345");
 
+function media(array) {
+    var total = 0;
+    array.forEach(function(elemento) {
+      total += elemento;
+    });
+    return total / array.length;
+  }
+
+  var semana1 = [77.53,80.00,140.00,174.00,210.00,159.00,172.80,172.20];
+  var mediaSem1 = media(semana1);
+  console.log(mediaSem1);
