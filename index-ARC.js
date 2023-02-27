392456ced9efc5cb18dcdd3f0526ccef3e45168d
Adolfo Rodriguez Cabezon <80678068+AdolfoRdgzCab@users.noreply.github.com>
2023-02-27 17:09:48 +0100

    Update index-ARC.js

--- index-ARC.js
+++ index-ARC.js
@@ -1,21 +1,4 @@
 
-
-function calcularMedia(array) {
-    let suma = 0;
-    array.forEach(function(elemento) {
-      suma += elemento;
-    });
-    return suma / array.length;
-  }
-
-  function calcularMedia(array) {
-    let suma = 0;
-    array.forEach(function(elemento) {
-      suma += elemento;
-    });
-    return suma / array.length;
-  }
-
   const miArray = [{state_id:4,state_s:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"31/01/2000",day	:31,temp_max:13.5,hormin_temp_max	:1736,temp_min	:10.67,hormin_temp_min	:2135,temp_average	:11.26,hum_max	:91.9,hum_min	:73.8,hum_average	:83.5,wind_speed:596,wind_direction:317.3,radiation:2,precipitation:0},
     {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"01/02/2000",day	:32,temp_max	:19.16,hormin_temp_max	:1411,temp_min	:7.94,hormin_temp_min	:147,temp_average	:13.28,hum_max	:98.5,hum_min	:47.41,hum_average	:80.5,wind_speed:967,wind_direction:273,radiation:11.76,precipitation:0},
     {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"02/02/2000",day	:33,temp_max	:20.1,hormin_temp_max	:1305,temp_min	:9.42,hormin_temp_min	:651,temp_average	:14.29,hum_max	:98.6,hum_min	:50.03,hum_average	:80.8,wind_speed:1.179,wind_direction:61.06,radiation:13.18,precipitation:0},
