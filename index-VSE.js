99ba300345de9f3740ea81867bf3192c32da96c6
vicsanesp <vse1997@gmail.com>
2023-02-27 16:40:22 +0100

    cambios en index

--- index-VSE.js
+++ index-VSE.js
@@ -11,15 +11,149 @@
 // app.listen(12345);
 
 // console.log("Server ready in port 12345");
+"Aceites de girasol"
+"REFINADO"	"S.E."	100 kg	CO-Córdoba	A.I.	77.53	76.55	76.00	76.00	76.00
+"REFINADO"	S.E.	100 kg	SE-Sevilla	A.I.	80.00	79.00	79.00	75.00	75.00
+"ACEITES DE OLIVA"									
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	GR-Montes Orientales	A.I.		180.00			180.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	GR-Alhama	A.I.	140.00	140.00	150.00	170.00	172.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	GR-Montes Occidentales	A.I.			179.00	180.00	183.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	CA-Cádiz	A.I.	174.00	174.00	176.00	176.00	180.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	HU-Huelva	A.I.	210.00	210.00			
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	GR-Granada	A.I.	159.00	168.00	169.00	175.00	180.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	CO-Córdoba	A.I.	172.80	172.80	178.70	180.00	182.00
+"	VÍRGENES-LAMPANTE (2 g)"	S.E.	100 kg	MA-Málaga	A.I.	172.20	173.60	177.10	183.30	180.70
 
-function media(array) {
-    var total = 0;
-    array.forEach(function(elemento) {
-      total += elemento;
-    });
-    return total / array.length;
-  }
-
-  var semana1 = [77.53,80.00,140.00,174.00,210.00,159.00,172.80,172.20];
-  var mediaSem1 = media(semana1);
+let array = [
+  {
+      product: "REFINADO",
+      type: "Aceites de girasol",
+      class: "S.E.",
+      unit: "100 kg",
+      market: "CO-Córdoba",
+      commpos: 'ADRA',
+      week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+      week2: 208,
+      week3: null,
+      week4: 503184.4737,
+      week5: 25830,
+  },
+  {
+    product: "REFINADO",
+    type: "Aceites de girasol",
+    class: "S.E.",
+    unit: "100 kg",
+    market: "SE-Sevilla",
+    commpos: 'ADRA',
+    week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+    week2: 208,
+    week3: null,
+    week4: 503184.4737,
+    week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "GR-Montes Orientales",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "GR-Alhama",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "GR-Montes Orientales",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "CA-Cádiz",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "HU-Huelva",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "GR-Granada",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "CO-Córdoba",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+},
+{
+  product: "VÍRGENES-LAMPANTE (2 g)",
+  type: "ACEITES DE OLIVA",
+  class: "S.E.",
+  unit: "100 kg",
+  market: "MA-Málaga",
+  commpos: 'ADRA',
+  week1: 'CAMPINGLAHABANA@HOTMAIL.COM',
+  week2: 208,
+  week3: null,
+  week4: 503184.4737,
+  week5: 25830,
+}
+];
   console.log(mediaSem1);
