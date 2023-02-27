6378ce3a26851166b74f958d7f887cb3c3a53758
unknown <adolexos@gmail.com>
2023-02-27 16:25:57 +0100

    commit

--- index-ARC.js
+++ index-ARC.js
@@ -8,6 +8,15 @@ function calcularMedia(array) {
     return suma / array.length;
   }
 
-  const miArray = [13.5,19.16,20.1,19.42,17.88,19.32,16.61,20.84,19.23,19.83]; //temperaturas maximas
+  const miArray = [{state_id:4,state_s:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"31/01/2000",day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"01/02/2000",day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:2,state_s	:"Huelva",station_id	:3,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},
+    {state_id	:3,state_s	:"Sevilla",station_id	:2,station_s	:"La Mojonera",date	:,day	:,temp_max	:,hormin_temp_max	:,temp_min	:,hormin_:,temp_min	:,temp_average	:,hum_max	:,hum_min	:,hum_average	:,wind_speed:,wind_direction:,radiation:,precipitation:},]; //temperaturas maximas
   const media = calcularMedia(miArray);
   console.log(media);
