e2811226a2a99765ae1deac40a70b5726323bce5
vicsanesp <vse1997@gmail.com>
2023-03-05 11:51:12 +0100

    añadido yaml y cambios en el index-VSE

--- index-VSE.js
+++ index-VSE.js
@@ -1,126 +1,128 @@
-// var express = require("express");
-// var cool = require("cool-ascii-faces");
+var express = require("express");
+var cool = require("cool-ascii-faces");
 
-// var app = express();
+var app = express();
+var port = process.env.PORT || 12345;
 
-// app.get("/faces", (req,res) =>{
-//     res.send(cool());
-//     console.log("New request");
-// });
+app.get("/faces", (req,res) =>{
+    res.send(cool());
+    console.log("New request");
+});
 
-// app.listen(12345);
+app.listen(port, ()=>{
+  console.log(`server ready in port ${port}`);
+});
+					
 
-// console.log("Server ready in port 12345");						
+// let array = [
+//   {
+//       product: "REFINADO",
+//       type: "Aceites de girasol",
+//       class: "S.E.",
+//       unit: "100 kg",
+//       market: "CO-Córdoba",
+//       commpos: "A.I.",
+//       week1: 77.53,
+//       week2: 76.55
+//   },
+//   {
+//     product: "REFINADO",
+//     type: "Aceites de girasol",
+//     class: "S.E.",
+//     unit: "100 kg",
+//     market: "SE-Sevilla",
+//     commpos: "A.I.",
+//     week1: 80.00,
+//     week2: 79.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "GR-Montes Orientales",
+//   commpos: "A.I.",
+//   week1: 180.00,
+//   week2: 180.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "GR-Alhama",
+//   commpos: "A.I.",
+//   week1: 140.00,
+//   week2: 140.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "GR-Montes Orientales",
+//   commpos: "A.I.",
+//   week1: 179.00,
+//   week2: 180.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "CA-Cádiz",
+//   commpos: "A.I.",
+//   week1: 174.00,
+//   week2: 174.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "HU-Huelva",
+//   commpos: "A.I.",
+//   week1: 210.00,
+//   week2: 210.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "GR-Granada",
+//   commpos: "A.I.",
+//   week1: 159.00,
+//   week2: 168.00
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "CO-Córdoba",
+//   commpos: "A.I.",
+//   week1: 172.80,
+//   week2: 172.80
+// },
+// {
+//   product: "VÍRGENES-LAMPANTE (2 g)",
+//   type: "ACEITES DE OLIVA",
+//   class: "S.E.",
+//   unit: "100 kg",
+//   market: "MA-Málaga",
+//   commpos: "A.I.",
+//   week1: 172.20,
+//   week2: 176.30
+// }
+// ];
 
-let array = [
-  {
-      product: "REFINADO",
-      type: "Aceites de girasol",
-      class: "S.E.",
-      unit: "100 kg",
-      market: "CO-Córdoba",
-      commpos: "A.I.",
-      week1: 77.53,
-      week2: 76.55
-  },
-  {
-    product: "REFINADO",
-    type: "Aceites de girasol",
-    class: "S.E.",
-    unit: "100 kg",
-    market: "SE-Sevilla",
-    commpos: "A.I.",
-    week1: 80.00,
-    week2: 79.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "GR-Montes Orientales",
-  commpos: "A.I.",
-  week1: 180.00,
-  week2: 180.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "GR-Alhama",
-  commpos: "A.I.",
-  week1: 140.00,
-  week2: 140.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "GR-Montes Orientales",
-  commpos: "A.I.",
-  week1: 179.00,
-  week2: 180.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "CA-Cádiz",
-  commpos: "A.I.",
-  week1: 174.00,
-  week2: 174.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "HU-Huelva",
-  commpos: "A.I.",
-  week1: 210.00,
-  week2: 210.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "GR-Granada",
-  commpos: "A.I.",
-  week1: 159.00,
-  week2: 168.00
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "CO-Córdoba",
-  commpos: "A.I.",
-  week1: 172.80,
-  week2: 172.80
-},
-{
-  product: "VÍRGENES-LAMPANTE (2 g)",
-  type: "ACEITES DE OLIVA",
-  class: "S.E.",
-  unit: "100 kg",
-  market: "MA-Málaga",
-  commpos: "A.I.",
-  week1: 172.20,
-  week2: 176.30
-}
-];
-
-function media(provincia) {
-  var filtrado = array.filter(v => v.market==provincia);
-  var suma=0;
-  for (let i=0;i<filtrado.length;i++){
-      var suma = suma + filtrado[i].week1;
-  }
-  return suma/filtrado.length;
-}
-  console.log(media("CO-Córdoba"));
+// function media(provincia) {
+//   var filtrado = array.filter(v => v.market==provincia);
+//   var suma=0;
+//   for (let i=0;i<filtrado.length;i++){
+//       var suma = suma + filtrado[i].week1;
+//   }
+//   return suma/filtrado.length;
+// }
+//   console.log(media("CO-Córdoba"));
