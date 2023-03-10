bff6be5600fd40a012080a9032b01e6dc3590861
unknown <adolexos@gmail.com>
2023-03-10 22:33:41 +0100

    Commit Adolfo

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var cool = require("cool-ascii-faces");
+const bodyParser = require("body-parser");
 
 var app = express();
 var port = process.env.PORT || 12345;
+app.use(bodyParser.json());
+
+const BASE_API_URL = "/api/v1";
 
 app.get("/cool", (request,response) =>{
     response.send(cool());
@@ -28,8 +32,102 @@ app.listen(port, ()=>{
   console.log(`server ready in port ${port}`);
 });
 
+//Get Adolfo
+app.get(BASE_API_URL + "/agrodata-almeria/", (req, res) => {
+  res.json(miArray);
+  console.log("New GET to /agrodata-almeria")
+});
 
-const miArray = [{state_id:4,state_s:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"31/01/2000",day	:31,temp_max:13.5,hormin_temp_max	:1736,temp_min	:10.67,hormin_temp_min	:2135,temp_average	:11.26,hum_max	:91.9,hum_min	:73.8,hum_average	:83.5,wind_speed:596,wind_direction:317.3,radiation:2,precipitation:0},
+
+//Array vacio + get Adolfo
+var miarray_10= [];
+
+app.get(BASE_API_URL + "/agrodata-almeria/loadInitialData", (req, res) => {
+  if (miarray_10.length==0){
+    miarray_10.push({state_id:4,state_s:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"31/01/2000",day	:31,temp_max:13.5,hormin_temp_max	:1736,temp_min	:10.67,hormin_temp_min	:2135,temp_average	:11.26,hum_max	:91.9,hum_min	:73.8,hum_average	:83.5,wind_speed:596,wind_direction:317.3,radiation:2,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"01/02/2000",day	:32,temp_max	:19.16,hormin_temp_max	:1411,temp_min	:7.94,hormin_temp_min	:147,temp_average	:13.28,hum_max	:98.5,hum_min	:47.41,hum_average	:80.5,wind_speed:967,wind_direction:273,radiation:11.76,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"02/02/2000",day	:33,temp_max	:20.1,hormin_temp_max	:1305,temp_min	:9.42,hormin_temp_min	:651,temp_average	:14.29,hum_max	:98.6,hum_min	:50.03,hum_average	:80.8,wind_speed:1.179,wind_direction:61.06,radiation:13.18,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"03/02/2000",day	:34,temp_max	:19.42,hormin_temp_max	:1332,temp_min	:10.49,hormin_temp_min	:530,temp_average	:14.68,hum_max	:91.7,hum_min	:53,hum_average	    :68.89,wind_speed:1.058,wind_direction:337.2,radiation:13.33,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"04/02/2000",day	:35,temp_max	:17.88,hormin_temp_max	:1336,temp_min	:11.04,hormin_temp_min	:430,temp_average	:14.43,hum_max	:92.2,hum_min	:18.54,hum_average	:58.5,wind_speed:2.325,wind_direction:50.62,radiation:14.11,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"05/02/2000",day	:36,temp_max	:19.32,hormin_temp_max	:959,temp_min	:9.56,hormin_temp_min	:2338,temp_average	:14.8,hum_max	:81.6,hum_min	:11.57,hum_average	:37.77,wind_speed:2.89,wind_direction:56.1,radiation:14.4,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"06/02/2000",day	:37,temp_max	:16.61,hormin_temp_max	:1254,temp_min	:7.75,hormin_temp_min	:451,temp_average	:12.22,hum_max	:94.9,hum_min	:43.88,hum_average	:65.21,wind_speed:1.571,wind_direction:302.5,radiation:13.91,precipitation:0},
+    {state_id	:2,state_s	:"Huelva",station_id	:3,station_s	:"La Mojonera",date	:"07/02/2000",day	:38,temp_max	:20.84,hormin_temp_max	:1351,temp_min	:8.88,hormin_temp_min	:646,temp_average	:13.96,hum_max	:95.9,hum_min	:25.49,hum_average	:72.2,wind_speed:1.294,wind_direction:37.55,radiation:13.85,precipitation:0},
+    {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"08/02/2000",day	:39,temp_max	:19.23,hormin_temp_max	:1405,temp_min	:10.16,hormin_temp_min	:435,temp_average	:14.42,hum_max	:91,hum_min	    :43.25,hum_average	:66.2,wind_speed:1.152,wind_direction:287,radiation:12.47,precipitation:0},
+    {state_id	:3,state_s	:"Sevilla",station_id	:2,station_s	:"La Mojonera",date	:"09/02/2000",day	:40,temp_max	:19.83,hormin_temp_max	:1355,temp_min	:10.29,hormin_temp_min	:609,temp_average	:14.68,hum_max	:94.9,hum_min	:36.78,hum_average	:66.35,wind_speed:1.676,wind_direction:60.25,radiation:13.9,precipitation:0}
+    );
+    res.json(miarray_10)
+    console.log("Se han creado 10 datos")
+
+} else {
+  res.send('Ya existen datos');
+    console.log('Ya existen datos')
+}
+});
+
+//Metodo Post en URL base Adolfo
+
+app.post(BASE_API_URL + "/agrodata-almeria", (req,res) => {
+  const keys = Object.keys(req.body);
+  if(keys.length<18){
+    res.status(400).send("No se han introducido datos suficientes");
+  } else{
+    const exists = miArray.some(agro => agro.state_id === req.body.state_id && agro.date === req.body.date)
+    if (exists) {
+      // Enviar una respuesta con un código de estado 409 Conflict si el objeto ya existe
+      res.status(409).send('Conflicto: Este objeto ya existe');
+    } else {
+      // Agregar los nuevos datos a la variable
+      miArray.push(req.body);
+      // Enviar una respuesta con un código de estado 201 Created
+      res.status(201).send('Los datos se han creado correctamente');
+    }
+  }
+  });
+
+  // Metodo PUT en URL base Adolfo
+app.put(BASE_API_URL + "/agrodata-almeria", (req, res) => {
+  res.status(405).send('En esta ruta no esta permitido el metodo PUT');
+});
+
+// Método DELETE en URL base Adolfo
+app.delete(BASE_API_URL + "/agrodata-almeria", (req, res) => {
+  miArray = [];
+  res.status(200).send('Se han borrado los datos');
+});
+
+//Metodo Post en loadInitialData Bloqueado Adolfo
+app.get(BASE_API_URL + "/agrodata-almeria/loadInitialData", (req, res) => {
+  res.status(405).send('En esta ruta no esta permitido el metodo POST');
+});
+
+//Metodo get en loadInitialData Adolfo
+
+app.get(BASE_API_URL + "/agrodata-almeria/loadInitialData", (req, res) => {
+  res.json(miarray_10);
+  res.status(200);
+});
+
+//Metodo Put en loadInitialData Adolfo
+app.put(BASE_API_URL + "/agrodata-almeria/loadInitialData", (req, res) => {
+if (!req.body) {
+  res.status(400).send('No se proporcionaron datos');
+} else {
+  miarray_10 = req.body;
+  res.status(200).send('Los datos se han actualizado correctamente');
+}
+});
+
+//Metodo delete en loadInitialData Adolfo
+app.delete(BASE_API_URL + "/agrodata-almeria/loadInitialData", (req, res) => {
+  miarray_10 = [];
+  res.status(200).send('Los datos se han borrado correctamente');
+});
+
+
+
+
+
+var miArray = [{state_id:4,state_s:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"31/01/2000",day	:31,temp_max:13.5,hormin_temp_max	:1736,temp_min	:10.67,hormin_temp_min	:2135,temp_average	:11.26,hum_max	:91.9,hum_min	:73.8,hum_average	:83.5,wind_speed:596,wind_direction:317.3,radiation:2,precipitation:0},
 {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"01/02/2000",day	:32,temp_max	:19.16,hormin_temp_max	:1411,temp_min	:7.94,hormin_temp_min	:147,temp_average	:13.28,hum_max	:98.5,hum_min	:47.41,hum_average	:80.5,wind_speed:967,wind_direction:273,radiation:11.76,precipitation:0},
 {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"02/02/2000",day	:33,temp_max	:20.1,hormin_temp_max	:1305,temp_min	:9.42,hormin_temp_min	:651,temp_average	:14.29,hum_max	:98.6,hum_min	:50.03,hum_average	:80.8,wind_speed:1.179,wind_direction:61.06,radiation:13.18,precipitation:0},
 {state_id	:4,state_s	:"Almería",station_id	:1,station_s	:"La Mojonera",date	:"03/02/2000",day	:34,temp_max	:19.42,hormin_temp_max	:1332,temp_min	:10.49,hormin_temp_min	:530,temp_average	:14.68,hum_max	:91.7,hum_min	:53,hum_average	    :68.89,wind_speed:1.058,wind_direction:337.2,radiation:13.33,precipitation:0},
