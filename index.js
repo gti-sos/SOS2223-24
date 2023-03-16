a63bc58492d220c9b4c6e265a18bd3220f394c70
vicsanesp <vse1997@gmail.com>
2023-03-16 16:09:46 +0100

    creacion de public y del html

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 
 var app = express();
 var port = process.env.PORT || 12345;
+
+app.use("/", express.static("./public"));
+
 app.use(bodyParser.json());
 
 var useOUA = require("./Samples/OUA");
