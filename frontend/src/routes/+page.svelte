873bb5dfe3d3dd80ae176740360fb21aff7c1423
vicsanesp <vse1997@gmail.com>
2023-04-17 16:27:58 +0200

    update minimo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -48,7 +48,7 @@
         <ul>
             <li><a href="{APIf}/agrodata-almeria">Frontend de agrodata-almeria</a> (developed by Adolfo Rodriguez Cabezon)</li>
             <li><a href="{APIf}/provisions-for-the-year-2014">Frontend de provisions-for-the-year-2014</a> (developed by Ouael Boussiali)</li>
-            <li><a href="{APIf}/agroprices-weekly">Frontend de agroprices-weekly</a> (developed by Víctor Manuel Sánchez Espada)</li>
+            <li><a href="https://sos2223-24.appspot.com/agroprices-weekly">Frontend de agroprices-weekly</a> (developed by Víctor Manuel Sánchez Espada)</li>
 
 
         </ul>
